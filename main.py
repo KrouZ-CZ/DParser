b3f62b1caf4110d663128b31b9b9b66a60d649e9
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-09-15 21:08:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from des import *
 from dparse import *
 
-
 class App(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.list = []
+        self.list = {'icons': {}, 'messages': []}
         self.ui.pushButton_4.clicked.connect(self.get)
         self.ui.pushButton.clicked.connect(self.save)
         self.ui.pushButton_2.clicked.connect(self.load)
@@ -30,24 +29,29 @@ class App(QtWidgets.QMainWindow):
         self.ui.pushButton_4.setEnabled(False)
         self.thread.start()
 
+    def draw_msg(self, i, icons):
+        item = QtWidgets.QListWidgetItem()
+
+        size = QtCore.QSize(45, 45)
+        pixmap_obj = QtGui.QPixmap()
+        pixmap_obj.loadFromData(icons[i['icon']])
+        icon = QtGui.QIcon(pixmap_obj)
+
+        time = i['timestamp'].split('T')[0] +' '+ i['timestamp'].split('T')[1].split('.')[0]
+        item.setIcon(icon)
+        item.setText(f"{i.get('username')} {time}:\n{i.get('text')}")
+        self.ui.listWidget.setIconSize(size)
+        self.ui.listWidget.addItem(item)
+
     def signal_handler(self, msg):
         self.thread.quit()
         self.signal.deleteLater()
+        for i in msg['icons']:
+            self.list['icons'][i] = msg['icons'][i]
+        self.list['messages'] += msg['messages']
         for i in msg['messages']:
-            self.list.append({'username': i['username'], 'text': i['text'], 'timestamp': i['timestamp']})
-        for i in msg['messages']:
-            item = QtWidgets.QListWidgetItem()
+            self.draw_msg(i, msg['icons'])
 
-            size = QtCore.QSize(45, 45)
-            pixmap_obj = QtGui.QPixmap()
-            pixmap_obj.loadFromData(msg['icons'][i['icon']])
-            icon = QtGui.QIcon(pixmap_obj)
-
-            time = i['timestamp'].split('T')[0] +' '+ i['timestamp'].split('T')[1].split('.')[0]
-            item.setIcon(icon)
-            item.setText(f"{i.get('username')} {time}:\n{i.get('text')}")
-            self.ui.listWidget.setIconSize(size)
-            self.ui.listWidget.addItem(item)
     def save(self):
         fp, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', filter='DAT files (*.dat)')
         with open(fp, 'wb') as f:
@@ -57,22 +61,11 @@ class App(QtWidgets.QMainWindow):
         fp, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', filter='DAT files (*.dat)')
         with open(fp, 'rb') as f:
             msg = pickle.load(f)
-
-        for i in msg:
-            item = QtWidgets.QListWidgetItem()
-
-            size = QtCore.QSize(45, 45)
-            icon = QtGui.QIcon('user.png')
-
-            time = i['timestamp'].split('T')[0] +' '+ i['timestamp'].split('T')[1].split('.')[0]
-            item.setIcon(icon)
-            item.setText(f"{i.get('username')} {time}:\n{i.get('text')}")
-            self.ui.listWidget.setIconSize(size)
-            self.ui.listWidget.addItem(item)
-
+        for i in msg['messages']:
+            self.draw_msg(i, msg['icons'])
     
     def clear(self):
-        self.list = []
+        self.list = {'icons': {}, 'messages': []}
         self.ui.listWidget.clear()
 
 if __name__ == "__main__":
